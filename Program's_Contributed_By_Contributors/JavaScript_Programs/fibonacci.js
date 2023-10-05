bbba530273c92c53d732892bd5a0a5792df07201
Shivam Lodhi <110722737+shivaml1194@users.noreply.github.com>
2023-10-06 03:49:20 +0530

    Create fibonacci.js
    Added a js file to print the Fibonacci series.

---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/fibonacci.js
@@ -0,0 +1,18 @@
+// A fibonacci sequence is written as:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+
+// program to generate fibonacci series up to n terms
+
+// take input from the user
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
